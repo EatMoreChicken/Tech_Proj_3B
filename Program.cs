558541c490dadc9576242da11125d6908f66147a
Varghese Charly <vcharly@usf.edu>
2020-09-22 15:31:33 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+
+Author: Varghese Charly
+Date: 09/22/2020
+Purpose: The purpose of this project is to practice using arrays. The project does not take any user input. The code will iterate through the
+specified array and print an output for every value in the array.
+
+*/
+
+using System;
+
+namespace Tech_Proj_3B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
